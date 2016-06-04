ef313dadc134f49a30705d49be4ac20d38e40a15
Armando Carlos Palmera Sánchez <acps91@gmail.com>
2016-06-04 19:54:30 +0000

    terminado pruebas unitarias de usuario

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :user do
+    email { "apalmeras91@gmail.com" }
+    password { "1234567" }
+    username { "acps" }
+    name { "armando" }
+    lastname { "palmera" }
+    
+    
+  end
+end
