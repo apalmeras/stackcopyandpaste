941666cdf2fea64c62685b7512a21517a7c660f0
Armando Carlos Palmera Sánchez <acps91@gmail.com>
2016-06-04 21:14:54 +0000

    categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604170008) do
+ActiveRecord::Schema.define(version: 20160604205402) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
